67de72d2ba9d8b1a199786be3732f565f235c9f1
Neslin Baydar <37474673+baydarn@users.noreply.github.com>
2020-02-07 02:32:12 +0300

    Update arac.java

--- src/arac.java
+++ src/arac.java
@@ -1,1 +1,34 @@
+public abstract class Arac implements IArac {
+	
+	protected int aracID;
+	protected String marka;
+	protected String yakitTuru;
+	protected int aracHizi;
+	protected int yolcuSayisi;
+	protected int tekerlekSayisi;
+	protected int fiyat;
+	protected int uretimYili;
+	protected String aracRengi;
+	
+	protected Arac() {
+	}
+	
+	protected Arac(int aracID, String marka, String yakitTuru, int aracHizi, int yolcuSayisi, int tekerlekSayisi, int fiyat, int uretimYili, String aracRengi) {
+		
+		this.aracID = aracID;
+		this.marka = marka;
+		this.yakitTuru = yakitTuru;
+		this.aracHizi = aracHizi;
+		this.yolcuSayisi = yolcuSayisi;
+		this.tekerlekSayisi = tekerlekSayisi;
+		this.fiyat = fiyat;
+		this.uretimYili = uretimYili;
+		this.aracRengi = aracRengi;
+
+	}
+	
+	public abstract String printInfo();
+	
+	
+}
 
